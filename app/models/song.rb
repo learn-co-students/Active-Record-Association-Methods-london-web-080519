c2f86f1dcb6df0063caa28c8265952dc7657a5e4
github username <github email address>
2019-08-18 18:15:57 +0100

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,7 +11,7 @@ class Song < ActiveRecord::Base
 
     drake = Artist.create(name: "Drake") if !drake
     
-    self.artist = drake
+    drake.songs << self 
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
